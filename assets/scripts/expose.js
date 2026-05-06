487ac26b3b7b3895f546f5363fb1fd627660b3be
AravVyawahare <arav17@gmail.com>
2026-05-05 22:20:31 -0700

    feat: query DOM elements and init JSConfetti in expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,11 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.querySelector('#horn-select');
+  const hornImg = document.querySelector('#expose > img');
+  const audio = document.querySelector('audio');
+  const slider = document.querySelector('#volume');
+  const volumeImg = document.querySelector('#volume-controls img');
+  const playBtn = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
 }
