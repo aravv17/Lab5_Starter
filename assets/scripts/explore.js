e3d22495b35d4fbc63690bbd37d5d79066bb50ee
AravVyawahare <arav17@gmail.com>
2026-05-05 23:31:33 -0700

    feat: query DOM elements and create utterance in explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,9 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const faceImg = document.querySelector('#explore img');
+  const textarea = document.querySelector('#text-to-speak');
+  const voiceSelect = document.querySelector('#voice-select');
+  const talkBtn = document.querySelector('#explore button');
+  const utterance = new SpeechSynthesisUtterance();
 }
