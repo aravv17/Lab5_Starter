dd766c9c290ef5ed952518d6a8b1ebe3197dc28a
AravVyawahare <arav17@gmail.com>
2026-05-05 22:22:13 -0700

    fix: use more specific DOM selectors in expose.js init

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,9 +5,9 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   const hornSelect = document.querySelector('#horn-select');
   const hornImg = document.querySelector('#expose > img');
-  const audio = document.querySelector('audio');
+  const audio = document.querySelector('#expose audio');
   const slider = document.querySelector('#volume');
-  const volumeImg = document.querySelector('#volume-controls img');
-  const playBtn = document.querySelector('button');
+  const volumeImg = document.querySelector('#volume-controls > img');
+  const playBtn = document.querySelector('#expose button');
   const jsConfetti = new JSConfetti();
 }
