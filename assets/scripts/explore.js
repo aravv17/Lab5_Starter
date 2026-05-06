c917dd4c0ac09eb828e88334144c8fe84b32f6a4
AravVyawahare <arav17@gmail.com>
2026-05-05 23:32:45 -0700

    feat: populate voice dropdown on load and voiceschanged

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,4 +8,17 @@ function init() {
   const voiceSelect = document.querySelector('#voice-select');
   const talkBtn = document.querySelector('#explore button');
   const utterance = new SpeechSynthesisUtterance();
+
+  const fillVoices = () => {
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+    speechSynthesis.getVoices().forEach(voice => {
+      const option = document.createElement('option');
+      option.value = voice.name;
+      option.textContent = voice.name;
+      voiceSelect.appendChild(option);
+    });
+  };
+
+  fillVoices();
+  speechSynthesis.addEventListener('voiceschanged', fillVoices);
 }
